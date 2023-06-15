4d6e7d296c4e824ad4af1ba11b67ed5a49dbfe44
Azzkaarr <imadc290@gmail.com>
2023-06-15 12:24:21 -0700

    tststs4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - printf "Programming is like building a multilingual puzzle, followed by a new line.
- *Return: Always 0.
+ * Return: Always 0.
  */
 
 int main(void)
@@ -11,4 +11,4 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 
-}
+} 
