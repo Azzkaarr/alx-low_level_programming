1b2264359767ce6bb06386c24666bd986aa86ae3
Azzkaarr <imadc290@gmail.com>
2023-06-16 19:42:43 -0700

    tttss1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,6 +4,7 @@
 /**
  * main - Prints the last digit of a randomly generated number
  * and whether it is greater than 5, less than 6, or 0.
+ * Last digit of 98 is 8 and is greate than 5.
  *
  * Return: Always 0
  */
