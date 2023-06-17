f7b48c8a03f8c03e68e6470f8dfcc76da82d961e
Azzkaarr <imadc290@gmail.com>
2023-06-16 18:32:09 -0700

    ttts3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,18 +4,20 @@
  *
  * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
 int lowerCase = 'a';
 int upperCase = 'A';
 while (lowerCase <= 'z')
 {
-putchar (lowerCase) ;
+putchar(lowerCase);
 lowerCase += 1;
 }
 while (upperCase <= 'Z')
-putchar (upperCase); 
+{
+putchar(upperCase);
 upperCase += 1;
-putchar ('\n');
+}
+putchar('\n');
 return (0);
 }
