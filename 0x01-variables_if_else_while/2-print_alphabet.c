df1cd0149371d4543f6aa7baed07e65e3f8dd029
Azzkaarr <imadc290@gmail.com>
2023-06-16 18:05:51 -0700

    ts2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry piont
+ *
+ * Return: Always 0 (success)
+ */
+int  main(void)
+{
+int lowerCASe ='a';
+while (lowerCASe <= 'z')
+{
+putchar(lowerCASe);
+lowerCASe += 1;
+}
+putchar ('\n');
+return (0);
+}
