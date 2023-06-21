bd2abe3cbe7efc61ddecc62203c9ad891d6cb0fc
Azzkaarr <imadc290@gmail.com>
2023-06-21 13:55:37 -0700

    ts0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,13 @@
 #include "main.h"
+
 /**
-* main - Entry point
-* Return: Always
-*/
+ * main - Entry point
+ * Return: Always 0
+ */
 
 int main(void)
 {
-_putchar(' ');
+_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
