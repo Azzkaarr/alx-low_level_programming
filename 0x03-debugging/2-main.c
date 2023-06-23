9b87521832e7b2e7f898101b961bd289bfedb64c
Azzkaarr <imadc290@gmail.com>
2023-06-23 12:14:54 -0700

    ts2

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -5,13 +5,15 @@
 * main - prints the largest of 3 integers
 * Return: 0
 */
-int largest_number(int a, int b, int c);
+
 int main(void)
 {
-      
+        int a, b, c;
         int largest;
 
-	 int a = 972, b = -89, c = 0;
+        a = 972;
+        b = -98;
+        c = 0;
 
         largest = largest_number(a, b, c);
 
